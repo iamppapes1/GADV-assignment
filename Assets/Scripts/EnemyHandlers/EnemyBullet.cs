d5469d26c697a02a14732822ce20e293b90904ee
iamppapes1 <iamppapes1@gmail.com>
2026-07-01 10:27:10 +0800

    Progressed bullets a little, added enemy handling

---
+++ Assets/Scripts/EnemyHandlers/EnemyBullet.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    private float _damage = 10;
+    private Rigidbody2D _rb;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _rb.AddForce(Vector2.down * 10);
+    }
+
+    public void Init(float damage)
+    {
+        _damage = damage;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Hit player");
+            PlaneMain plane = other.GetComponent<PlaneMain>();
+            plane.TakeDamage(_damage);
+            Destroy(gameObject);
+        }
+        
+    }
+}
