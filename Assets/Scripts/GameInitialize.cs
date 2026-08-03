d52f461756913e02f130f2fad712de6c22cba786
iamppapes1 <iamppapes1@gmail.com>
2026-08-03 11:52:47 +0800

    added enemy spawning yay

--- Assets/Scripts/GameInitialize.cs
+++ Assets/Scripts/GameInitialize.cs
@@ -1,16 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameInitialize : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector2 _Min = new Vector2(-2.8f, 4f);
+    private Vector2 _Max = new Vector2(2.8f, 4f);
+    private GameObject[] _Enemies;
+
     void Start()
     {
-        
+        _Enemies = Resources.LoadAll<GameObject>("Prefabs/Enemies");
+       Coroutine Spawner =  StartCoroutine(SpawnEnemies());
+        StopCoroutine(Spawner);
     }
 
     // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemies()
     {
-        
+        while (true)
+        {
+            float spawnLocation = Random.Range(-2.8f, 2.8f);
+            GameObject randomEnemy = _Enemies[Random.Range(0, _Enemies.Length - 1)];
+
+            GameObject Enemy = Instantiate(
+                randomEnemy,
+                new Vector2(spawnLocation, 4f),
+                randomEnemy.transform.rotation
+            );
+
+            Debug.Log("Spawned");
+
+            yield return new WaitForSeconds(1);
+        }        
     }
 }
